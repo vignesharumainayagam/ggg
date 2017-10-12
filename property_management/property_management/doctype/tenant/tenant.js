0b1224e646a837b2c98b26fc306ae80e3b27cdaa
vignesh <vigneshwaran.valiantsystems.com>
2017-10-12 15:05:26 +0530

    changes

--- property_management/property_management/doctype/tenant/tenant.js
+++ property_management/property_management/doctype/tenant/tenant.js
@@ -37,3 +37,49 @@ frappe.ui.form.on("Tenant", "onload", function(frm) {
   });
 
 });
+
+frappe.ui.form.on("Tenant", "onload", function(frm) {
+  cur_frm.fields_dict.rent_per_mont.$input.on("keypress", function(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+         if (charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+     else
+         return true;
+  });
+
+});
+frappe.ui.form.on("Tenant", "onload", function(frm) {
+  cur_frm.fields_dict.maintenance_per_month.$input.on("keypress", function(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+         if (charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+     else
+         return true;
+  });
+
+});
+frappe.ui.form.on("Tenant", "onload", function(frm) {
+  cur_frm.fields_dict.rate_per_unit.$input.on("keypress", function(evt) {
+      var theEvent = evt || window.event;
+      var key = theEvent.keyCode || theEvent.which;
+      key = String.fromCharCode( key );
+      var regex = /[0-9]|\./;
+      if( !regex.test(key) ) {
+        theEvent.returnValue = false;
+        if(theEvent.preventDefault) theEvent.preventDefault();
+      }
+
+  });
+
+  });
+
+frappe.ui.form.on("Tenant", "onload", function(frm) {
+  cur_frm.fields_dict.deposit_paid.$input.on("keypress", function(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode
+         if (charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+     else
+         return true;
+  });
+
+});
