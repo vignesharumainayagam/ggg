b1ed49ee0ae5aa4ce052d638a833e43b53a235ba
vignesh <vigneshwaran.valiantsystems.com>
2017-10-24 15:42:59 +0530

    gog

--- property_management/property_management/doctype/expense_type/expense_type.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, Valiant Systems and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Expense Type', {
-	refresh: function(frm) {
-
-	}
-});
